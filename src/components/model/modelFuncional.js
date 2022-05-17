ba348d9badde52ac07869355f59ee6596ff6c7a4
Facu <facundo.mendez@live.com.ar>
2022-05-17 14:59:55 -0300

    .

--- src/components/model/modelFuncional.js
+++ src/components/model/modelFuncional.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import TablaModel from "./src/models/Tabla.glb"
+import TablaModel from "./src/models/tabla3d.gltf"
 import BaseModel from "./src/models/Base.glb"
 
 const modelFuncional= () =>{
